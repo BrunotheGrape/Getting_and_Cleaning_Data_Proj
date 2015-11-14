9acf02b8a28426fa29815e0604b9852369e3b3ef
Boyd Ingalls <boyd.ingalls@gmail.com>
2015-11-14 15:54:56 -0600

    adding activity column

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,5 @@
 require(data.table)
+require(dplyr)
 
 tsbjx <- fread("test/X_test.txt")
 tsbjy <- fread("test/Y_test.txt")
@@ -8,5 +9,8 @@ trnsbjy <- fread("train/Y_train.txt")
 trnsbjt <- fread("train/subject_train.txt")
 varNames <- fread("features.txt")
 varNames <- varNames$V2
+actNames <- fread("activity_labels.txt")
 setnames(tsbjx, varNames)
 setnames(trnsbjx, varNames)
+actcol <- merge(tsbjy, actNames, by.x="V1",by.y="V1" )
+
