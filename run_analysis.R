792ec9f09f4e4a2c23d6d8e7dce1a1cde6ef43d4
Boyd Ingalls <boyd.ingalls@gmail.com>
2015-11-15 10:51:16 -0600

    final commented version

--- run_analysis.R
+++ run_analysis.R
@@ -37,8 +37,9 @@ trnsbjf <- cbind(trnsbja, trnsbjx)
 prod <- rbind(tsbjf, trnsbjf)
 
         #create tidy dataframe
-nms <- c(id, anm, varNames)
-tidy.data <- data.frame()
+nms <- c(id, anm, varNames) # creating naming vector
+
+tidy.data <- data.frame() #estblishing data frames for future use
 tidy.m <- data.frame()
 m <- data.frame()
 col1 <- data.frame()
@@ -49,27 +50,34 @@ act <- data.frame()
 
         # subsetting data and extracting column means
 
-for (i in 1:30) {
+for (i in 1:30) { # for loop to loop through test subject id numbers
 sbst1 <- filter(prod, test_sbj_id == i) # subset by test subject
-for (l in 1:6) {
+for (l in 1:6) { # for loop to loop through activities by number
 sbst2 <- filter(sbst1, activity_no == l) # subset by activity
 sbcol1 <- select(sbst2, -(test_sbj_id:activity_name)) #subset for activity columns
-m <- colMeans(sbcol1, na.rm = TRUE)
-m <- t(m)
-m <- as.data.frame(m)
-tidy.m <- rbind(tidy.m, m)
-sbj <- i
+m <- colMeans(sbcol1, na.rm = TRUE) # calculating the column means and storing it in "m"
+m <- t(m) # reorienting m as a column
+m <- as.data.frame(m) # redifining m as a data frame
+tidy.m <- rbind(tidy.m, m) # using row bind to store reults in "tidy.m"
+sbj <- i # storing the test subject id
 sbjid <- rbind(sbjid, sbj )
-act <- l
+act <- l # storing the activity number
 actid <- rbind(actid, act)
 
 }
 }
 
-tidy.data <- tidy.m
-setnames(sbjid, "test_sbj_id")
-setnames(actid, "activity_no")
+        # assembly the data into the "tidy.data" product
+
+tidy.data <- tidy.m # storing the column means in the "tidy.data" data frame
+
+        #naming the columns of "tidy.data" before binding them into a single object
+setnames(sbjid, "test_sbj_id") 
+setnames(actid, "activity_no") 
 setnames(tidy.data, varNames)
+
+        # combines the collected data into a single data frame called "tidy.data"
+
 tdactcoltst <- merge(actid, actNames, by.x = "activity_no", by.y = "activity_no" )
 tidy.data <- cbind(tdactcoltst, tidy.data)
 tidy.data <- cbind(sbjid, tidy.data)
