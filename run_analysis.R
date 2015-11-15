c38299d03c902aee6e468c4f65a8301d0f0beedb
Boyd Ingalls <boyd.ingalls@gmail.com>
2015-11-14 19:00:12 -0600

    data combined into one frame

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,38 @@
+        # loading required packages
 require(data.table)
 require(dplyr)
-
-tsbjx <- fread("test/X_test.txt")
-tsbjy <- fread("test/Y_test.txt")
+        #creating column names vectors
+ano <- c("activity_no")
+anm <- c("activity_no", "activity_name")
+id <- c("test_sbj_id", "test_or_train")
+        #reading in the data
+tsbjx <- fread("test/X_test.txt") 
+tsbjy <- fread("test/Y_test.txt", col.names = ano)
 tsbjt <- fread("test/subject_test.txt")
 trnsbjx <- fread("train/X_train.txt")
-trnsbjy <- fread("train/Y_train.txt")
+trnsbjy <- fread("train/Y_train.txt", col.names = ano)
 trnsbjt <- fread("train/subject_train.txt")
 varNames <- fread("features.txt")
+actNames <- fread("activity_labels.txt", col.names = anm)
+        #creating additional columns
+actcoltst <- merge(tsbjy, actNames, by.x = "activity_no", by.y = "activity_no" ) #creates activity names columns
+actcoltrn <- merge(trnsbjy, actNames, by.x = "activity_no", by.y = "activity_no" )
+
+tsbjt <- cbind(tsbjt, "test") #adds data for tst_or_trn to test subject id columns
+trnsbjt <- cbind(trnsbjt, "train")
+setnames(tsbjt, id)
+setnames(trnsbjt, id)
+tsbja <- cbind(tsbjt, actcoltst) # combines test subject id with activity names 
+trnsbja <- cbind(trnsbjt, actcoltrn)
+        #naming activity variable columns
+
 varNames <- varNames$V2
-actNames <- fread("activity_labels.txt")
 setnames(tsbjx, varNames)
 setnames(trnsbjx, varNames)
-actcol <- merge(tsbjy, actNames, by.x="V1",by.y="V1" )
+
+        #creating combined data set
+
+tsbjf <- cbind(tsbja, tsbjx)
+trnsbjf <- cbind(trnsbja, trnsbjx)
+prod <- rbind(tsbjf, trnsbjf)
 
